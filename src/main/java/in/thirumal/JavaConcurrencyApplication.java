e956e1b1e7f0e29a8da8fa5be8dd8a2f58dea8f8
thirumal <m.thirumal@hotmail.com>
2020-07-25 13:53:39 +0530

    Initial Commit

---
+++ src/main/java/in/thirumal/JavaConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package in.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConcurrencyApplication.class, args);
+	}
+
+}
