bb9f3dd79a3dea0a986ca45bdfefbdc4c16a1220
thirumal <m.thirumal@hotmail.com>
2020-08-02 16:14:14 +0530

    Simple maven

---
+++ src/main/java/in/thirumal/forkjoin/ForKJoin.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package in.thirumal.forkjoin;
+
+/**
+ * @author thirumal
+ *
+ */
+public class ForKJoin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
