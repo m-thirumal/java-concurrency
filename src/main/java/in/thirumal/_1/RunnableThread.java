fa1bbe8f3016adcb2b226c98710c3eb8610d1f91
Thirumal <m.thirumal@hotmail.com>
2020-09-16 18:43:28 +0530

    Simple Thread concept

---
+++ src/main/java/in/thirumal/_1/RunnableThread.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package in.thirumal._1;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author thirumal
+ *
+ */
+public class RunnableThread {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Thread t = new Thread(()-> IntStream.range(0, 12).forEach(a-> System.out.println(a + " " + Thread.currentThread().getName())));
+		t.start();
+		
+		Thread r1 = new Thread(new RunnableImplement());
+		
+		Thread r2 = new Thread(new RunnableImplement());
+		
+		
+		Thread r3 = new Thread (() -> {
+			IntStream.range(0, 12).forEach(a-> System.out.println(a + " " + Thread.currentThread().getName()));
+		});
+		
+		r1.start();
+		r2.start();
+		r3.start();
+	}
+
+}
+
+class RunnableImplement implements Runnable {
+	
+	@Override
+	public void run() {
+		IntStream.range(0, 12).forEach(a-> System.out.println(a + " " + Thread.currentThread().getName()));
+	}
+}
---
+++ src/main/java/in/thirumal/_1/SimpleThread.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package in.thirumal._1;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author thirumal
+ *
+ */
+public class SimpleThread {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ExtendThread e = new ExtendThread();
+		ExtendThread e1 = new ExtendThread();
+		e.start();
+		e1.start();
+
+	}
+
+}
+
+class ExtendThread extends Thread {
+	@Override
+	public void run() {
+		IntStream.range(0, 500).forEach( a-> System.out.println(a + " " + Thread.currentThread().getName()));
+	}
+}
