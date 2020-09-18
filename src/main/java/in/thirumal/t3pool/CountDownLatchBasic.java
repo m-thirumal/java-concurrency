8889f461596513a2bcb58341178d393b9d11110e
Thirumal <m.thirumal@hotmail.com>
2020-09-18 14:37:03 +0530

    CountDownlatch

---
+++ src/main/java/in/thirumal/t3pool/CountDownLatchBasic.java
@@ -0,0 +1,58 @@
+	/**
+ * 
+ */
+package in.thirumal.t3pool;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author thirumal
+ *
+ */
+public class CountDownLatchBasic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CountDownLatch countDownLatch = new CountDownLatch(3);
+
+		ExecutorService executorService = Executors.newFixedThreadPool(3);
+		for (int i = 0; i < 3; i++) {
+			executorService.execute(new Task1(countDownLatch));
+		}
+		try {
+			countDownLatch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Completed....");
+	}
+
+} 
+
+class Task1 implements Runnable {
+
+	private CountDownLatch latch;
+	
+	
+	public Task1(CountDownLatch latch) {
+		this.latch = latch;
+	}
+
+
+	@Override
+	public void run() {
+		System.out.println("Started...");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		latch.countDown();
+	}
+	
+}
