b746283b04af7edc7d8b135694a5fba9ede306b0
thirumal <m.thirumal@hotmail.com>
2020-09-23 06:46:18 +0530

    Added Tree

---
+++ src/main/java/in/thirumal/forkjoin/Tree.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package in.thirumal.forkjoin;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author thirumal
+ *
+ */
+public class Tree implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6786485032465445280L;
+
+	public static Tree[] newTree(int size) {
+		Tree[] trees = new Tree[size];
+		for (int i = 0; i < size; i++) {
+			trees[i] = new Tree("🌳#" + i);
+		}
+		return trees;
+	}
+	
+	private String treeName;
+	private int fruitCount;
+	private String fruitPicker;
+	
+	public Tree(String treeName) {
+		super();
+		this.treeName = treeName;
+		this.fruitCount = 3;
+	}
+	
+	public int pickFruit(String fruitPicker) {
+		try {
+			//System.out.printf("%s started picking apples from %s \n", workerName, treeLabel);
+			TimeUnit.SECONDS.sleep(1);
+			System.out.printf("%s picked %d 🍏s from %s \n", fruitPicker, fruitCount, treeName);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return fruitCount;
+	}
+	
+	
+	
+}
