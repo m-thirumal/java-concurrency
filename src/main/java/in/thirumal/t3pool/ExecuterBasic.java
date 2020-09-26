55126e2278637c100a29101abdebd89904cbf206
Thirumal <m.thirumal@hotmail.com>
2020-09-26 19:28:39 +0530

    executor

--- src/main/java/in/thirumal/t3pool/ExecuterBasic.java
+++ src/main/java/in/thirumal/t3pool/ExecuterBasic.java
@@ -5,6 +5,9 @@ package in.thirumal.t3pool;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -20,11 +23,36 @@ public class ExecuterBasic {
 	 */
 	public static void main(String[] args) {
 		
-		ExecutorService executorService = Executors.newFixedThreadPool(2);
+//		ThreadFactory threadFactory = new ThreadFactory() {
+//
+//			@Override
+//			public Thread newThread(Runnable r) {
+//				Thread thread = new Thread();
+//				thread.setPriority(Thread.MAX_PRIORITY);
+//				
+//				return thread;
+//			}
+//			
+//		};
+//		
+		
+//		ExecutorService executorService = Executors.newFixedThreadPool(2, threadFactory);
+		
+		//ExecutorService executorService = Executors.newSingleThreadExecutor();
+		
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		
+		ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(12);
+		
+		ScheduledFuture<?> remainder = scheduledExecutorService.scheduleAtFixedRate(()->{
+			System.out.println("hello");
+		}, 0, 1, TimeUnit.SECONDS);
+		
 		
 		for (int i = 0; i < 5; i++) {
 			executorService.submit(new Task(i));
 		}
+		System.out.println(remainder.isDone());
 		executorService.shutdown();
 		
 		try {
