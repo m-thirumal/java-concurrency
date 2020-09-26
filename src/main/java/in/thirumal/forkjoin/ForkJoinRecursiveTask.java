d4816ce7397f94372a941580bd27f9c6361f2b37
Thirumal <m.thirumal@hotmail.com>
2020-09-26 14:51:21 +0530

    Cancel, exception are added

--- src/main/java/in/thirumal/forkjoin/ForkJoinRecursiveTask.java
+++ src/main/java/in/thirumal/forkjoin/ForkJoinRecursiveTask.java
@@ -3,8 +3,12 @@
  */
 package in.thirumal.forkjoin;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 /**
@@ -21,16 +25,43 @@ public class ForkJoinRecursiveTask {
 		ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
 		
 		PickFruitTask pickFruitTask = new PickFruitTask(trees, 0, trees.length - 1);
+		long startTime  = System.currentTimeMillis();
 		
-		int result = forkJoinPool.invoke(pickFruitTask);
+//		int result = forkJoinPool.invoke(pickFruitTask);
+//		System.out.println(result + " => Fruites are collected successfully");
+//		
 		
-		System.out.println(result + " => Fruites are collected successfully");
+//		forkJoinPool.execute(pickFruitTask);
+//	
+//		
+//		try {
+//			forkJoinPool.awaitTermination(2, TimeUnit.MINUTES);
+//		} catch (InterruptedException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		
+		//==============With Future and submit
+		Future<Integer> result = forkJoinPool.submit(pickFruitTask);
+		try {
+			forkJoinPool.awaitTermination(2, TimeUnit.MINUTES);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			System.out.println(result.get() + " => Fruites are collected successfully");
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Total Time taken: " + (System.currentTimeMillis() - startTime));
 		
 	}
 
 }
 
-class PickFruitTask extends RecursiveTask<Integer>{
+class PickFruitTask extends RecursiveTask<Integer> { //Use "RecursiveAction" if don't want to return the result or RecursiveTas<Void>
 	
 	/**
 	 * 
@@ -53,11 +84,18 @@ class PickFruitTask extends RecursiveTask<Integer>{
 		if (endInclusive - startInclusive < threashold) {
 			return doCompute();
 		}
+//		if (startInclusive > trees.length/2) {
+//			int i = 4 / 0;
+//			completeExceptionally(new CustomException());
+//		}
+		
 		int midpoint = startInclusive + (endInclusive - startInclusive) / 2;
 		PickFruitTask leftPickFruitTask = new PickFruitTask(trees, startInclusive, midpoint);
 		PickFruitTask rightPickFruitTask = new PickFruitTask(trees, midpoint + 1, endInclusive);
 		
-		rightPickFruitTask.fork();
+		rightPickFruitTask.fork(); //Compute: is synchronous and Fork: is asynchronous. Order must be fork and compute, otherwise it will execute in synchronous
+		
+		//rightPickFruitTask.cancel(true);
 		
 		return leftPickFruitTask.compute() + rightPickFruitTask.join();
 	}
@@ -67,4 +105,12 @@ class PickFruitTask extends RecursiveTask<Integer>{
 				.map(i->trees[i].pickFruit())
 				.sum();
 	}
+}
+
+class CustomException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -538473976148737307L;
 }
